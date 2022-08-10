af8d103db2470d0c477d630015d5b2b3719ad6dd
Sergio Bateca <sbtk87@hotmail.com>
2022-08-10 15:58:17 -0500

    Feature: Implement size test and workflow

--- app/test/fixtures/order.py
+++ app/test/fixtures/order.py
@@ -1,7 +1,7 @@
 import pytest
 
 from ..utils.functions import (shuffle_list, get_random_sequence,
-                               get_random_string)
+                               get_random_string, get_random_date, get_random_price)
 
 
 def client_data_mock() -> dict:
@@ -9,7 +9,8 @@ def client_data_mock() -> dict:
         'client_address': get_random_string(),
         'client_dni': get_random_sequence(),
         'client_name': get_random_string(),
-        'client_phone': get_random_sequence()
+        'client_phone': get_random_sequence(),
+        'date': get_random_date()
     }
 
 
@@ -24,14 +25,27 @@ def client_data():
 
 
 @pytest.fixture
-def order(create_ingredients, create_size, client_data) -> dict:
+def order(create_ingredients, create_sizes, client_data) -> dict:
     ingredients = [ingredient.get('_id') for ingredient in create_ingredients]
-    size_id = create_size.get('_id')
+    sizes = [size.get('_id') for size in create_sizes]
+    size_id = sizes[0].get('_id')
     return {
         **client_data_mock(),
-        'ingredients': ingredients,
+        'total_price': get_random_price(),
+        'ingredients': shuffle_list(ingredients)[:5],
         'size_id': size_id
     }
+    
+@pytest.fixture
+def create_order(client, order_uri, create_ingredients, create_sizes) -> dict:
+    ingredients = [ingredient.get('_id') for ingredient in create_ingredients]
+    sizes = [size.get('_id') for size in create_sizes]
+    response = client.post(order_uri, json={
+            **client_data_mock(),
+            'ingredients': shuffle_list(ingredients)[:5],
+            'size_id': sizes[0]
+        })
+    return response
 
 
 @pytest.fixture
